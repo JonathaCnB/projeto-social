177e88f868c192a4684c75702f012f15742ba79c
arthur <sousanot.23@gmail.om>
2021-07-08 18:47:06 -0300

    Adicionando os campos no admin

--- users/admin.py
+++ users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 
 from .forms import UserChangeForm, UserCreationForm
-from .models import User
+from .models import User, UserProfile
 
 
 @admin.register(User)
@@ -10,3 +10,7 @@ class UserAdmin(auth_admin.UserAdmin):
     form = UserChangeForm
     add_form = UserCreationForm
     model = User
+
+@admin.register(UserProfile)
+class UserPrfielAdmin(admin.ModelAdmin):
+    pass
